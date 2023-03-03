96a63ae3cac7b2a1665570ccaf7fe752482324a0
YusufTOPRAK <tprkysf21@gmail.com>
2023-03-03 15:45:51 +0300

    Add project files.

---
+++ fonksiyon_ile_toplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fonksiyon_ile_toplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2;
+            Console.Write("Sayı Gir: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Sayı Gir: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            var result = topla(sayi1,sayi2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static int topla(int s1, int s2)
+        {
+            return s1 + s2;
+        }
+
+    }
+}
